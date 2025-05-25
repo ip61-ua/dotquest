455a5fe343f7c4eae5ad5508042ba2a541aa27c9
ip61-ua <ip61@alu.ua.es>
2025-05-25 08:42:15 +0200

    implement Model.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-Console.WriteLine("Hello, World!");
+using Model;
+
+namespace DotQuest
+{
+    public class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            var test = new Question("ESTO ES UN TEST", []);
+            /**
+              for (int i = 0; i < args.Length; i++)
+                  Console.Write(args[i] + "\n");
+            */
+            Console.Write(test);
+        }
+    }
+}
